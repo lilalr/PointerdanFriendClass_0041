7bf035d5132bccb80ff1cae8d6a67aa0b49087ec
lilalr <gurennut@gmail.com>
2024-05-08 11:56:47 +0700

    menambah int main

--- PointerFriend/PointerFriend.cpp
+++ PointerFriend/PointerFriend.cpp
@@ -9,3 +9,17 @@ public:
 	}
 };
 
+int main()
+{
+	mahasiswa mhs{ 1 };
+	mhs.showNim();
+
+	mahasiswa& refMhs = mhs;
+	refMhs.nim = 2;
+	mhs.showNim();
+
+	mahasiswa* pMhs = &mhs;
+	pMhs->nim = 3;
+	pMhs->showNim();
+	return 0;
+}
