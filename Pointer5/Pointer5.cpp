58a4d15d5e54bb107244b27343ed88894924c46d
lilalr <gurennut@gmail.com>
2024-05-08 12:02:18 +0700

    menambah class pelajar

--- Pointer5/Pointer5.cpp
+++ Pointer5/Pointer5.cpp
@@ -7,3 +7,11 @@ public:
     void showNilaiPelajar(pelajar& x);
 };
 
+class pelajar {
+private:
+    int nilai;
+public:
+    pelajar() { nilai = 100; }
+    friend void manusia::showNilaiPelajar(pelajar& x);
+};
+
